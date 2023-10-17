0e2b8366349dc9afa32673d273585ef27b7b43a2
ahmaddynugroho <ahmaddwi700@gmail.com>
2023-10-17 21:44:21 +0700

    fix: lsp-setup & smart-splits conflict

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 -- Table of Content
+-- lsp_setup_conflit
 -- toc_lazy
 
 vim.loader.enable()
@@ -209,28 +210,15 @@ local lz_telescope = {
 }
 local lz_lsp_setup = {
 	"junnplus/lsp-setup.nvim",
-	priority = 100,
 	dependencies = {
 		"neovim/nvim-lspconfig",
 		"williamboman/mason.nvim", -- optional
 		"williamboman/mason-lspconfig.nvim", -- optional
 	},
 	opts = {
-		default_mappings = true,
+        default_mappings = true,
 		mappings = {
-			gD = "lua vim.lsp.buf.declaration()",
-			gd = "lua vim.lsp.buf.definition()",
-			gt = "lua vim.lsp.buf.type_definition()",
-			gi = "lua vim.lsp.buf.implementation()",
-			gr = "lua vim.lsp.buf.references()",
-			K = "lua vim.lsp.buf.hover()",
-			-- ['<C-k>'] = 'lua vim.lsp.buf.signature_help()', -- conflict with lz_smart_splits
-			["<space>rn"] = "lua vim.lsp.buf.rename()",
-			["<space>ca"] = "lua vim.lsp.buf.code_action()",
-			-- ['<space>f'] = 'lua vim.lsp.buf.formatting()', -- i have
-			["<space>e"] = "lua vim.diagnostic.open_float()",
-			["[d"] = "lua vim.diagnostic.goto_prev()",
-			["]d"] = "lua vim.diagnostic.goto_next()",
+			['<C-k>'] = 'lua require("smart-splits").move_cursor_up()', -- lsp_setup_conflit
 		},
 		on_attach = function()
 			require("lsp-setup.utils").format_on_save(nil)
@@ -512,7 +500,7 @@ local lz_smart_splits = {
 		-- moving between splits
 		vim.keymap.set("n", "<C-h>", require("smart-splits").move_cursor_left)
 		vim.keymap.set("n", "<C-j>", require("smart-splits").move_cursor_down)
-		vim.keymap.set("n", "<C-k>", require("smart-splits").move_cursor_up)
+        -- see lsp_setup_conflit
 		vim.keymap.set("n", "<C-l>", require("smart-splits").move_cursor_right)
 		-- swapping buffers between windows
 		vim.keymap.set("n", "<leader><leader>h", require("smart-splits").swap_buf_left)
