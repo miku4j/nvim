43808b88cc0e6cdc0147639ba70050ac1d935f9a
ahmaddynugroho <ahmaddwi700@gmail.com>
2024-11-05 14:45:53 +0700

    use poimandres, disable word highlight

--- custom.lua
+++ custom.lua
@@ -1,7 +1,18 @@
 return {
 	{ "nvim-lualine/lualine.nvim", enabled = false },
 	{ "echasnovski/mini.statusline", version = "*", opts = {} },
-  { "jeetsukumaran/vim-indentwise" },
+  {
+    "jeetsukumaran/vim-indentwise",
+    event = "VeryLazy",
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      document_highlight = {
+        enabled = false,
+      },
+    },
+  },
 	{
 		"kylechui/nvim-surround",
 		version = "*",
@@ -15,12 +26,26 @@ return {
 	},
 	{
 		"zenbones-theme/zenbones.nvim",
+    enabled = false,
 		dependencies = "rktjmp/lush.nvim",
 		lazy = false,
 		priority = 1000,
 		config = function()
 			vim.g.neobones = { transparent_background = true }
-			vim.cmd.colorscheme("neobones")
+			-- vim.cmd.colorscheme("neobones")
 		end,
 	},
+  {
+    "olivercederborg/poimandres.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("poimandres").setup({
+        disable_background = true,
+        disable_float_background = true,
+        disable_italics = true,
+      })
+      vim.cmd.colorscheme("poimandres")
+    end,
+  },
 }
