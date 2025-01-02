961f1eda008da9af2c016eef1bf2e759a60ed82d
ahmaddynugroho <ahmaddwi700@gmail.com>
2025-01-02 11:37:45 +0700

    feat: transparent night owl theme

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -3,6 +3,9 @@ return {
     "oxfist/night-owl.nvim",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
+    opts = {
+      transparent_background = true
+    }
     -- config = function()
     --   require("night-owl").setup()
     --   vim.cmd.colorscheme("night-owl")
