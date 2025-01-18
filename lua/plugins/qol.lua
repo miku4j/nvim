8111323587c428ceefec1cdcdcd4a9398bbd6234
ahmaddynugroho <ahmaddwi700@gmail.com>
2025-01-18 23:54:22 +0700

    feat: add supermaven, disable annoying settings

--- lua/plugins/qol.lua
+++ lua/plugins/qol.lua
@@ -1,4 +1,26 @@
 return {
+  -- disable annoying settings
+  {
+    "folke/noice.nvim",
+    opts = {
+      lsp = {
+        signature = { auto_open = { enabled = false } },
+      },
+    },
+  },
+  {
+    "saghen/blink.cmp",
+    opts = {
+      completion = {
+        documentation = {
+          auto_show = false,
+        },
+        ghost_text = {
+          enabled = false,
+        },
+      },
+    },
+  },
   {
     "jeetsukumaran/vim-indentwise",
     vscode = true,
