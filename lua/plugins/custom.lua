88bdda6d50b3441bfc0eac52d1463d659721b1db
ahmaddynugroho <ahmaddwi700@gmail.com>
2025-01-18 22:42:31 +0700

    feat: enable both custom theme

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -1,15 +1,16 @@
 return {
   {
     "oxfist/night-owl.nvim",
-    enabled = false,
+    enabled = true,
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    priority = 1001, -- make sure to load this before all the other start plugins
     opts = {
       transparent_background = true,
     },
   },
   {
     "folke/tokyonight.nvim",
+    enabled = true,
     lazy = false,
     priority = 1000,
     opts = {
@@ -19,7 +20,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight-night",
+      colorscheme = "night-owl",
     },
   },
 }
--- lua/plugins/qol.lua
+++ lua/plugins/qol.lua
@@ -1,10 +1,12 @@
 return {
   {
     "jeetsukumaran/vim-indentwise",
+    vscode = true,
     event = "VeryLazy",
   },
   {
     "kylechui/nvim-surround",
+    vscode = true,
     version = "*",
     event = "VeryLazy",
     opts = {},
