839be4d9eb7db6899786de64ddc8d05580c2f7a0
ahmaddynugroho <ahmaddwi700@gmail.com>
2025-01-22 13:14:32 +0700

    feat(quick-scope): custom highlight

--- lua/plugins/qol.lua
+++ lua/plugins/qol.lua
@@ -38,6 +38,12 @@ return {
   {
     "unblevable/quick-scope",
     event = "VeryLazy",
+    config = function()
+      vim.cmd([[
+        highlight QuickScopePrimary guifg='#afff5f' gui=underline ctermfg=155 cterm=underline
+        highlight QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline
+      ]])
+    end,
   },
 
   -- remap git signs
