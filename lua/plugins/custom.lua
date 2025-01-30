e5820c667c56fd777f5f9827b1496b16dd80f724
ahmaddynugroho <ahmaddwi700@gmail.com>
2025-01-30 09:49:17 +0700

    feat(theme): use onedarkpro

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -1,27 +1,13 @@
 return {
   {
-    "oxfist/night-owl.nvim",
-    enabled = true,
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1001, -- make sure to load this before all the other start plugins
-    opts = {
-      transparent_background = true,
-    },
-  },
-  {
-    "folke/tokyonight.nvim",
-    enabled = true,
-    lazy = false,
-    priority = 1000,
-    opts = {
-      transparent = true,
-    },
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "night-owl",
-    },
+    "olimorris/onedarkpro.nvim",
+    priority = 1000, -- Ensure it loads first
+    config = function()
+      require("onedarkpro").setup({
+        options = { transparency = true },
+      })
+      vim.cmd("colorscheme onedark_dark")
+    end,
   },
   {
     "nvim-neo-tree/neo-tree.nvim",
