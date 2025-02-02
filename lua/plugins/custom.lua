9cd7295c38ed5563c4aea2e6f1d5e9c2a381d157
ahmaddynugroho <ahmaddwi700@gmail.com>
2025-02-02 23:24:14 +0700

    feat(theme): use tokyonight-night

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -1,18 +1,14 @@
 return {
   {
-    "olimorris/onedarkpro.nvim",
-    priority = 1000, -- Ensure it loads first
-    config = function()
-      require("onedarkpro").setup({
-        options = { transparency = true },
-      })
-      vim.cmd("colorscheme onedark_dark")
-    end,
-  },
-  {
     "nvim-neo-tree/neo-tree.nvim",
     opts = {
       window = { width = 30 },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-night",
     },
   },
 }
