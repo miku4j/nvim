78ed4b5204d807357e080f5fbc2b7b13a315ad07
ahmaddynugroho <ahmaddwi700@gmail.com>
2025-02-20 23:57:59 +0700

    feat(keymap): add aider keymap

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,13 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = LazyVim.safe_keymap_set
+
+-- Aider; mnemonic: Intelligent
+map("n", "<C-i>", function()
+  Snacks.terminal.toggle("aider --watch-files")
+end, { desc = "Open Aider Terminal" })
+map("t", "<C-i>", function()
+  Snacks.terminal.toggle("aider --watch-files")
+end, { desc = "Close Aider Terminal" })
