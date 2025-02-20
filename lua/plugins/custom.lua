57d492c5b5871cfa8037abe495f233fe8d767e9d
ahmaddynugroho <ahmaddwi700@gmail.com>
2025-02-20 23:57:49 +0700

    feat(theme): add transparency to ayu

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -9,7 +9,19 @@ return {
     "Shatur/neovim-ayu",
     priority = 1000,
     config = function()
-      require("ayu").setup({})
+      require("ayu").setup({
+        overrides = {
+          Normal = { bg = "None" },
+          NormalFloat = { bg = "none" },
+          ColorColumn = { bg = "None" },
+          SignColumn = { bg = "None" },
+          Folded = { bg = "None" },
+          FoldColumn = { bg = "None" },
+          CursorLine = { bg = "None" },
+          CursorColumn = { bg = "None" },
+          VertSplit = { bg = "None" },
+        },
+      })
       require("ayu").colorscheme()
     end,
   },
