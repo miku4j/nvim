b0eb12f2c07b78706107805d92571f03d06fb4a1
ahmaddynugroho <ahmaddwi700@gmail.com>
2025-03-15 20:38:13 +0700

    feat(fold): remove nvim-origami dependency
    * removed nvim-origami from dependencies

--- lua/plugins/fold.lua
+++ lua/plugins/fold.lua
@@ -2,7 +2,7 @@ return {
   {
     "kevinhwang91/nvim-ufo",
     event = "VeryLazy",
-    dependencies = { "kevinhwang91/promise-async", { "chrisgrieser/nvim-origami", opts = {} } },
+    dependencies = { "kevinhwang91/promise-async" },
     config = function()
       --- https://github.com/kevinhwang91/nvim-ufo
       require("ufo").setup({
