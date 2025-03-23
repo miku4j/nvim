706dbe41726acda067bf327f394db9c931dc14d1
ahmaddynugroho <ahmaddwi700@gmail.com>
2025-03-23 12:46:06 +0700

    feat(git): add git-conflict.nvim, add lackluster theme

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -22,7 +22,30 @@ return {
           VertSplit = { bg = "None" },
         },
       })
-      require("ayu").colorscheme()
+      -- require("ayu").colorscheme()
     end,
+  },
+  {
+    "slugbyte/lackluster.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      tweak_background = {
+        -- normal = 'none',
+      }
+    }
+    -- init = function()
+    --   vim.cmd.colorscheme("lackluster")
+    --   vim.cmd.colorscheme("lackluster-hack") -- my favorite
+    --   vim.cmd.colorscheme("lackluster-mint")
+    -- end,
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "lackluster-hack",
+      colorscheme = "ayu",
+    },
   },
 }
