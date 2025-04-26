b99d34dd7aba0a4680649384ea3ecd31fcd584f9
miku4j <ahmaddwi700@gmail.com>
2025-04-27 00:23:40 +0700

    feat: add flash

---
+++ lua/custom/plugins/flash.lua
@@ -0,0 +1,14 @@
+return {
+  'folke/flash.nvim',
+  event = 'VeryLazy',
+  ---@type Flash.Config
+  opts = {
+    modes = {
+      char = { enabled = false },
+    },
+  },
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+  },
+}
