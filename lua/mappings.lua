c5811ee232c4b89f57d0a34cad98d7ea6ada3f88
miku4j <ahmaddwi700@gmail.com>
2025-05-02 00:59:52 +0700

    feat: add vim-indentwise

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -3,7 +3,8 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
+local nomap = vim.keymap.del
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+nomap('n', '<A-h>') -- for vim-indentwise
---
+++ lua/plugins/vim-indentwise.lua
@@ -0,0 +1,7 @@
+return {
+  "jeetsukumaran/vim-indentwise",
+  keys = {
+    { "<A-h>", "<Plug>(IndentWisePreviousEqualIndent)", desc = "Go to previous equal indent" },
+    { "<A-l>", "<Plug>(IndentWiseNextEqualIndent)", desc = "Go to next equal indent" },
+  }
+}
