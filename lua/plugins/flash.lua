6d6ed263597343e4314ec1f069570b3f14e10286
miku4j <ahmaddwi700@gmail.com>
2025-05-02 01:04:23 +0700

    feat: add flash

---
+++ lua/plugins/flash.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/flash.nvim",
+  opts = {
+    modes = {
+      char = {
+        enabled = false
+      }
+    }
+  },
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+  }
+}
