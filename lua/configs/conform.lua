388221f862e94084a63d707927ef9e3498329dcd
miku4j <ahmaddwi700@gmail.com>
2025-05-02 10:17:37 +0700

    feat(conform): add async format

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,8 +1,10 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    css = { "prettier" },
+    html = { "prettier" },
+    vue = { "prettier" },
+    js = { "prettier" },
   },
 
   -- format_on_save = {
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -5,6 +5,11 @@ require "nvchad.mappings"
 local map = vim.keymap.set
 local nomap = vim.keymap.del
 
+nomap("n", "<A-h>") -- for vim-indentwise
+nomap("n", "<leader>fm") -- for async conform
+
 map("n", ";", ":", { desc = "CMD enter command mode" })
 
-nomap('n', '<A-h>') -- for vim-indentwise
+map("n", "<leader>fm", function()
+  require("conform").format { async = true }
+end, { desc = "Async format buffer" })
