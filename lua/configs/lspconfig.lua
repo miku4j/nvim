98f8751880cafd90d37370359c23099d125b2858
miku4j <ahmaddwi700@gmail.com>
2025-05-02 15:16:02 +0700

    feat: add eslint

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -2,5 +2,5 @@ require("nvchad.configs.lspconfig").defaults()
 
 require "lsp.volar"
 
-local servers = { "html", "cssls", "tailwindcss" }
+local servers = { "html", "cssls", "tailwindcss", "eslint" }
 vim.lsp.enable(servers)
