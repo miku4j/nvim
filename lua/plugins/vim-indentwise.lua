4ca89f3072ca3dc31923e178a395e68c2502091e
miku4j <ahmaddwi700@gmail.com>
2025-05-05 13:03:21 +0700

    feat: add verylazy event

--- lua/plugins/vim-indentwise.lua
+++ lua/plugins/vim-indentwise.lua
@@ -1,5 +1,6 @@
 return {
   "jeetsukumaran/vim-indentwise",
+  event = "VeryLazy",
   keys = {
     { "<A-h>", "<Plug>(IndentWisePreviousEqualIndent)", desc = "Go to previous equal indent", mode = { "n", "x" } },
     { "<A-l>", "<Plug>(IndentWiseNextEqualIndent)", desc = "Go to next equal indent", mode = { "n", "x" } },
