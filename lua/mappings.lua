2682db0562aba81e0c80f5f8bd3b542bc47affca
miku4j <ahmaddwi700@gmail.com>
2025-05-06 10:35:07 +0700

    fix: <C-i> not jumping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -13,3 +13,9 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("n", "<leader>fm", function()
   require("conform").format { async = true }
 end, { desc = "Async format buffer" })
+
+-- fix <C-i> not jumping
+nomap("n", "<TAB>")
+nomap("n", "<S-TAB>")
+map("n", "H", ":bprev<CR>", { desc = "Prev buffer" }) -- don't need H and L coz of flash.nvim
+map("n", "L", ":bnext<CR>", { desc = "Next buffer" })
