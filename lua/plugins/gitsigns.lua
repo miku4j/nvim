0d2c412deb87c6de57cbb48cc18f7b70f3a0dc68
miku4j <ahmaddwi700@gmail.com>
2025-05-10 14:32:42 +0700

    feat: add gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,12 +1,15 @@
 return { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
-    opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
-    },
+    config = function ()
+      require('gitsigns').setup{
+        signs = {
+          add = { text = '+' },
+          change = { text = '~' },
+          delete = { text = '_' },
+          topdelete = { text = '‾' },
+          changedelete = { text = '~' },
+        },
+        on_attach = require("config.mappings.gitsigns")
+      }
+    end
   }
