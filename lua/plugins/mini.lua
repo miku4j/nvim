ce80c7dc8a564999d0e8cdbcce125a76f646c5f7
miku4j <ahmaddwi700@gmail.com>
2025-05-11 16:11:38 +0700

    feat: add mini.pairs

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -32,6 +32,8 @@ return { -- Collection of various small independent plugins/modules
 
     require('mini.tabline').setup { show_icons = false }
 
+    require('mini.pairs').setup {}
+
     -- Simple and easy statusline.
     --  You could remove this setup call if you don't like it,
     --  and try some other statusline plugin
