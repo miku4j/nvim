c40274357fe67a2094beb1643fe6d6438ee1040d
miku4j <ahmaddwi700@gmail.com>
2025-05-11 17:17:17 +0700

    feat: add wrapped line hjkl

--- lua/config/mappings/vim.lua
+++ lua/config/mappings/vim.lua
@@ -7,8 +7,15 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- buffer
 vim.keymap.set('n', 'H', '<cmd>bprev<CR>')
 vim.keymap.set('n', 'L', '<cmd>bnext<CR>')
 vim.keymap.set('n', '<A-c>', '<cmd>bnext<CR>')
 vim.keymap.set('n', '<leader>bo', '<cmd>silent! execute "%bd|e#|bd#"<CR>')
 vim.keymap.set('n', '<leader>x', '<cmd>bd<CR>')
+
+-- window
+vim.keymap.set('n', 'j', 'gj', { noremap = true })
+vim.keymap.set('n', 'k', 'gk', { noremap = true })
+vim.keymap.set('n', 'h', 'gh', { noremap = true })
+vim.keymap.set('n', 'l', 'gl', { noremap = true })
