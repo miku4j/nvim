ca0af369d43ac4329e77451bbd3cb9845d34e1ff
miku4j <ahmaddwi700@gmail.com>
2025-05-11 17:36:59 +0700

    feat(telescope): add command history mapping

--- lua/config/mappings/telescope.lua
+++ lua/config/mappings/telescope.lua
@@ -10,6 +10,7 @@ vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iag
 vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
 vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 vim.keymap.set('n', '<leader>sb', builtin.buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>s;', builtin.command_history, { desc = 'Command history' })
 
 -- Slightly advanced example of overriding default behavior and theme
 vim.keymap.set('n', '<leader>/', function()
