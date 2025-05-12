129907b5855bf4101d8c4fae548ab1f1bf0775d9
miku4j <ahmaddwi700@gmail.com>
2025-05-12 15:44:02 +0700

    clean lazyvim

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,14 +1,8 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
 -- Add any additional autocmds here
-
--- local function augroup(name)
---   return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
--- end
-
--- vim.api.nvim_create_autocmd("BufReadPost", {
---   group = augroup("last_loc"),
---   callback = function(event)
---     vim.cmd("Neotree show")
---   end,
--- })
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
--- lua/plugins/tscontext.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "nvim-treesitter/nvim-treesitter-context",
-    opts = function()
-      vim.api.nvim_set_hl(0, "TreesitterContext", { bg = "none" })
-      return {}
-    end,
-  },
-}
