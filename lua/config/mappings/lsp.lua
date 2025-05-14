0b1c88b1fefe12a115d1ba8211c266fcf010a983
miku4j <ahmaddwi700@gmail.com>
2025-05-14 10:30:14 +0700

    feat(lsp-mappings): add comprehensive LSP keybindings with descriptive list
    - introduced mappings for:
      - references with telescope
      - renaming via LSP
      - navigating to implementations
      - going to definitions and declarations
      - opening document and workspace symbols
      - type definitions
      - code actions
    - refactored to loop through `mappings` table for cleaner setup
    - improved maintainability and clarity of keybinding configurations

--- lua/config/mappings/lsp.lua
+++ lua/config/mappings/lsp.lua
@@ -1,15 +1,70 @@
-vim.keymap.set('n', '<leader>lr', require('telescope.builtin').lsp_references, { desc = '[G]oto [R]eferences' })
+local mappings = {
+  -- LSP references
+  {
+    '<leader>lr',
+    require('telescope.builtin').lsp_references,
+    '[G]oto [R]eferences',
+  },
+  -- Rename
+  {
+    '<leader>lR',
+    vim.lsp.buf.rename,
+    'Rename (LSP)',
+  },
+  -- Implementations
+  {
+    '<leader>li',
+    require('telescope.builtin').lsp_implementations,
+    '[G]oto [I]mplementation',
+  },
+  -- Definitions
+  {
+    '<leader>ld',
+    require('telescope.builtin').lsp_definitions,
+    '[G]oto [D]efinitions',
+  },
+  -- Declaration
+  {
+    '<leader>lD',
+    vim.lsp.buf.declaration,
+    '[G]oto [D]eclaration',
+  },
+  -- Document symbols
+  {
+    '<leader>ls',
+    require('telescope.builtin').lsp_document_symbols,
+    'Open Document Symbols',
+  },
+  -- Workspace symbols
+  {
+    '<leader>lw',
+    require('telescope.builtin').lsp_workspace_symbols,
+    'Open Workspace Symbols',
+  },
+  -- Workspace symbols (dynamic)
+  {
+    '<leader>lW',
+    require('telescope.builtin').lsp_dynamic_workspace_symbols,
+    'Open Workspace Symbols',
+  },
+  -- Type definitions
+  {
+    '<leader>lt',
+    require('telescope.builtin').lsp_type_definitions,
+    '[G]oto [T]ype Definition',
+  },
+  -- Code actions
+  {
+    '<leader>la',
+    vim.lsp.buf.code_action,
+    'Code Actions',
+  },
+}
 
-vim.keymap.set('n', '<leader>li', require('telescope.builtin').lsp_implementations, { desc = '[G]oto [I]mplementation' })
-
-vim.keymap.set('n', '<leader>ld', require('telescope.builtin').lsp_definitions, { desc = '[G]oto [D]efinitions' })
-
-vim.keymap.set('n', '<leader>lD', vim.lsp.buf.declaration, { desc = '[G]oto [D]eclaration' })
-
-vim.keymap.set('n', '<leader>ls', require('telescope.builtin').lsp_document_symbols, { desc = 'Open Document Symbols' })
-
-vim.keymap.set('n', '<leader>lw', require('telescope.builtin').lsp_workspace_symbols, { desc = 'Open Workspace Symbols' })
-
-vim.keymap.set('n', '<leader>lW', require('telescope.builtin').lsp_dynamic_workspace_symbols, { desc = 'Open Workspace Symbols' })
-
-vim.keymap.set('n', '<leader>lt', require('telescope.builtin').lsp_type_definitions, { desc = '[G]oto [T]ype Definition' })
+-- Loop through mappings to set keybindings
+for _, map in ipairs(mappings) do
+  local key = map[1]
+  local func = map[2]
+  local desc = map[3]
+  vim.keymap.set('n', key, func, { desc = desc })
+end
