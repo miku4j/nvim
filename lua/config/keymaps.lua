793e584ecbd0e5b9dfbefc354bdde75565766ca9
miku4j <ahmaddwi700@gmail.com>
2025-06-05 10:08:29 +0700

    fix(keymap): swap next/prev hunk

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,12 +2,12 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.del("n", "<A-j>")
-vim.keymap.set("n", "<A-j>", function()
+vim.keymap.del("n", "<A-k>")
+vim.keymap.set("n", "<A-k>", function()
   require("gitsigns").nav_hunk("prev")
 end, { desc = "Go to previous hunk" })
 
-vim.keymap.del("n", "<A-k>")
-vim.keymap.set("n", "<A-k>", function()
+vim.keymap.del("n", "<A-j>")
+vim.keymap.set("n", "<A-j>", function()
   require("gitsigns").nav_hunk("next")
 end, { desc = "Go to next hunk" })
