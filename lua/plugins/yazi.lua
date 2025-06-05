436bfaf1eb97fd74a689b06fcc558d8e573baaaa
miku4j <ahmaddwi700@gmail.com>
2025-06-05 09:38:08 +0700

    feat(plugin-yazi): integrate yazi.nvim terminal file manager
    - Add `yazi.nvim` plugin for enhanced file management within Neovim.
      - includes `folke/snacks.nvim` as a required dependency.
      - configured with the following keymaps:
        - `<leader>yo`: open yazi targeting the current file's directory.
        - `<leader>yr`: resume or toggle the last yazi session.
      - initial options set:
        - `open_for_directories` disabled by default.
        - help keymap (`show_help`) set to `<f1>`.

---
+++ lua/plugins/yazi.lua
@@ -0,0 +1,29 @@
+return {
+  "mikavilpas/yazi.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    -- check the installation instructions at
+    -- https://github.com/folke/snacks.nvim
+    "folke/snacks.nvim",
+  },
+  keys = {
+    {
+      "<leader>yo",
+      mode = { "n", "v" },
+      "<cmd>Yazi<cr>",
+      desc = "[O]pen yazi at the current file",
+    },
+    {
+      "<leader>yr",
+      "<cmd>Yazi toggle<cr>",
+      desc = "[R]esume the last yazi session",
+    },
+  },
+  opts = {
+    -- if you want to open yazi instead of netrw, see below for more info
+    open_for_directories = false,
+    keymaps = {
+      show_help = "<f1>",
+    },
+  },
+}
