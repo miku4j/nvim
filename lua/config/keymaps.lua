33c76cf2b49ea56971148312bf627d5a8549c9ff
miku4j <ahmaddwi700@gmail.com>
2025-06-05 13:37:19 +0700

    feat(config): add tailwind support and new keymaps
    - Enabled Tailwind language support plugin
      - Added 'lazyvim.plugins.extras.lang.tailwind' to `lazyvim.json`
    
    - Updated and added keymaps in `lua/config/keymaps.lua`
      - Gitsigns navigation:
        - Reconfigured 'previous hunk' keymap (<A-k>)
          - Ensures `require("gitsigns").nav_hunk("prev")` is used
        - Reconfigured 'next hunk' keymap (<A-j>)
          - Ensures `require("gitsigns").nav_hunk("next")` is used
      - Added new keymap:
        - 'copy current buffer' (<leader>bc)
          - Maps to `ggyG''` for copying the entire buffer content

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,12 +2,17 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- previous hunk
 vim.keymap.del("n", "<A-k>")
 vim.keymap.set("n", "<A-k>", function()
   require("gitsigns").nav_hunk("prev")
 end, { desc = "Go to previous hunk" })
 
+-- next hunk
 vim.keymap.del("n", "<A-j>")
 vim.keymap.set("n", "<A-j>", function()
   require("gitsigns").nav_hunk("next")
 end, { desc = "Go to next hunk" })
+
+-- copy current buffer
+vim.keymap.set("n", "<leader>bc", "ggyG''", { desc = "Copy current buffer" })
