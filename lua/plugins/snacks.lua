e7980cabe6c3aebdd39ad820440048cb56b91d70
miku4j <ahmaddwi700@gmail.com>
2025-06-21 12:54:22 +0700

    feat(plugins): add new plugins and update dependencies
    - Add new plugins via LazyVim extras
      - luasnip for snippets
      - yanky for improved yank/paste
      - mini-hipatterns for highlighting TODOs, etc.
      - eslint for linting
    - Remove unused markdown plugins
      - markdown-preview.nvim
      - render-markdown.nvim
    - Configure snacks.nvim
      - Enable the 'bigfile' module
    - Update all other plugins to their latest versions

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,6 +2,7 @@ return {
   "snacks.nvim",
   opts = {
     indent = { enabled = false }, -- disable indent line
-    words = { enabled = false} -- disable word highlight under cursor
+    words = { enabled = false}, -- disable word highlight under cursor
+    bigfile = {}
   },
 }
