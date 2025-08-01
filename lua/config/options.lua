8dc966441143081f12109a86e1606130347fde65
miku4j <ahmaddwi700@gmail.com>
2025-08-02 00:10:10 +0700

    fix: remove treesitter temporary fix

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,10 +10,6 @@ vim.g.autoformat = false
 vim.g.snacks_animate = false
 vim.o.smoothscroll = false
 
--- temporary fix for treesitter flickering on multiple window
--- see https://github.com/neovim/neovim/issues/32660
-vim.g._ts_force_sync_parsing = true
-
 -- line wrap
 vim.o.wrap = true
 vim.o.breakindent = true
