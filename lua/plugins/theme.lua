762df33b0b99931cb689b10513bf35dabfef5f22
miku4j <ahmaddwi700@gmail.com>
2025-10-30 11:07:28 +0700

    feat: use official kanso theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -9,7 +9,20 @@ return {
         transparent_background = true,
       })
 
-      vim.cmd.colorscheme("catppuccin-mocha")
+      -- vim.cmd.colorscheme("catppuccin-mocha")
+    end,
+  },
+  {
+    "webhooked/kanso.nvim",
+    lazy = false,
+    priority = 1001,
+    config = function()
+      require("kanso").setup({
+        italics = false, -- enable italics
+        transparent = true, -- do not set background color
+      })
+
+      vim.cmd("colorscheme kanso-zen")
     end,
   },
 }
